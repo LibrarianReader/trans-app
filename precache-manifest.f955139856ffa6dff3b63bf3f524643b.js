87b31ad1c3f0a9bea0a9e331b25f2cf9c36647e4
Reader <mail81025@mail.ru>
2025-09-27 02:32:57 +0300

    Initial commit

---
+++ precache-manifest.f955139856ffa6dff3b63bf3f524643b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "db8ba13204b43983b3019a1869e7d445",
+    "url": "/trans-app/index.html"
+  },
+  {
+    "revision": "9665c8b079bc54ee36e5",
+    "url": "/trans-app/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "fa2d8ee02cf184fc4314",
+    "url": "/trans-app/static/js/2.3ad75f56.chunk.js"
+  },
+  {
+    "revision": "9665c8b079bc54ee36e5",
+    "url": "/trans-app/static/js/main.4aa5ada0.chunk.js"
+  },
+  {
+    "revision": "c7517b7be6c3f112e7fb",
+    "url": "/trans-app/static/js/runtime~main.f6217a61.js"
+  }
+]);
